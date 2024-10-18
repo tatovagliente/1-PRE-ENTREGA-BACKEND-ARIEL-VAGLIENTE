fd0247a86de41dce1b278b6176cb3bbba90eaa2f
Ariel <ariel.vagliente@yahoo.com.ar>
2024-10-17 21:38:09 -0300

    Ruteo correctamente a CartManager y funcionando

--- src/Dao/cartManager.js
+++ src/Dao/cartManager.js
@@ -1,14 +1,12 @@
 const fs = require('fs').promises;
-let rutaArchivoCart = "./src/Data/carts.json";
-
 
 class CartManager {
+    #path = "";
     constructor(rutaArchivoCart) {
-        this.path = rutaArchivoCart; 
+        this.#path = rutaArchivoCart;
         this.idInicial = 1;
     }
 
-    
     async createCart() {
         const carts = await this.getCarts();
         const newCart = {
@@ -21,30 +19,29 @@ class CartManager {
         return newCart;
     }
 
-    
     async addProductToCart(cartId, product) {
         const carts = await this.getCarts();
         const cart = carts.find((c) => c.id === cartId);
         if (!cart) {
             throw new Error("Carrito no encontrado");
         }
-
         cart.products.push(product);
         await this.saveCarts(carts);
     }
 
-    
     async getCarts() {
         try {
-            const data = await fs.readFile(this.path, 'utf-8');
+            const data = await fs.readFile(this.#path, 'utf-8');
             return JSON.parse(data);
         } catch (error) {
+            console.log("Error leyendo el archivo:", error);
             return [];
         }
     }
 
     async saveCarts(carts) {
-        await fs.writeFile(this.path, JSON.stringify(carts, null, 2));
+        await fs.writeFile(this.#path, JSON.stringify(carts, null, 2));
     }
 }
 
+module.exports = { CartManager };
--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require ("express");
 const { ProductManager} = require("./Dao/ProductManager");
+const { CartManager } = require("./Dao/cartManager");
 
 
 const PORT = 8080
 
 const productManager = new ProductManager ("./src/Data/products.json")
+const cartManager= new CartManager ("./src/Data/carts.json")
 
 const app = express()
 
@@ -18,11 +20,27 @@ app.get("/contacto", (req, res)=>{
 })
 
 app.get("/products", async (req, res)=>{
-
-    let products = await productManager.getProducts("./src/Data/products.json")
-
+    try{
+        let products = await productManager.getProducts()
+        console.log(products)
     res.send(products)
+    } catch (error){
+        res.send(`Error interno del Server!`)
+    }
+    
 })
+
+app.get("/carts", async (req, res)=>{
+    try{
+        let cart = await cartManager.getCarts()
+
+    res.send(cart)
+    } catch (error){
+        res.send(`Error interno del Server!`)
+    }
+    
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`Server up en puerto ${PORT} con Express`)
